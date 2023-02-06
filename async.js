37850f47999c3e5c7b8a6b79d832a117bbc65f87
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-07 02:36:01 +0900

    Advanced JS - Promise

--- async.js
+++ async.js
@@ -1,12 +1,21 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
   let fileData;
+  // callback
+  // fs.readFile("data.txt", function (error, fileData) {
+  //   console.log("File parsing done!");
+  //   console.log(fileData.toString());
+  // });
 
-  fs.readFile("data.txt", function (error, fileData) {
-    console.log("File parsing done!");
-    console.log(fileData.toString());
-  });
+  // promise
+  fs.readFile("data.txt")
+    .then(function (fileData) {
+      console.log("File parsing done!");
+      console.log(fileData.toString());
+      // return anotherAsyncOperation
+    })
+    .then(function () {});
 
   console.log("Hi there!");
 }
